5d9b4238252cffe67fb87c2e79edeedcc61819b9
soo <hwansoo-kim@rsupport.com>
2017-11-29 13:31:20 +0900

    cross os path apply

--- config/webpack/loaders.js
+++ config/webpack/loaders.js
@@ -1,62 +1,63 @@
 const { repoPath, confPath, srcPath } = require('../../lib/path')
+const path = require('path')
 
 /**
  * Loaders
  * @namespace Loaders
  */
 const Loaders = {
-  loaderList: {
-    eslint: {
-      test: /\.js$/,
-      include: [srcPath],
-      loader: 'eslint-loader',
-      options: {
-        configFile: `${repoPath}/.eslintrc.json`
-      }
-    },
-    javascript: {
-      test: /\.js$/,
-      include: [srcPath],
-      use: ['babel-loader']
-    },
-    style: 'style-loader',
-    css: {
-      loader: 'css-loader',
-      options: {
-        sourceMap: true,
-        module: true,
-        importLoaders: 1,
-        localIdentName: '[local]--[hash:base64:5]',
-        camelCase: 'only'
-      }
-    },
-    postcss: {
-      loader: 'postcss-loader',
-      options: {
-        config: {
-          path: `${confPath}/postcss.config.js`
+    loaderList: {
+        eslint: {
+            test: /\.js$/,
+            include: [srcPath],
+            loader: 'eslint-loader',
+            options: {
+                configFile: path.join(repoPath, '.eslintrc.json')
+            }
+        },
+        javascript: {
+            test: /\.js$/,
+            include: [srcPath],
+            use: ['babel-loader']
+        },
+        style: 'style-loader',
+        css: {
+            loader: 'css-loader',
+            options: {
+                sourceMap: true,
+                module: true,
+                importLoaders: 1,
+                localIdentName: '[local]--[hash:base64:5]',
+                camelCase: 'only'
+            }
+        },
+        postcss: {
+            loader: 'postcss-loader',
+            options: {
+                config: {
+                    path: path.join(confPath, 'postcss.config.js')
+                }
+            }
         }
-      }
+    },
+
+    /**
+     * Get loaders
+     * @param  {String}       type
+     * @return {Array|String}
+     */
+    get (type) {
+        const loaders = type.split(' ').map(t => this.loaderList[t]).filter(t => !!t)
+        let loader
+
+        if (loaders.length === 0) {
+            console.log(`No '${type}' loader!`)
+        } else if (loaders.length === 1) {
+            [loader] = loaders
+        }
+
+        return loader || loaders
     }
-  },
-
-  /**
-   * Get loaders
-   * @param  {String}       type
-   * @return {Array|String}
-   */
-  get (type) {
-    const loaders = type.split(' ').map(t => this.loaderList[t]).filter(t => !!t)
-    let loader
-
-    if (loaders.length === 0) {
-      console.log(`No '${type}' loader!`)
-    } else if (loaders.length === 1) {
-      [loader] = loaders
-    }
-
-    return loader || loaders
-  }
 }
 
 module.exports = Loaders
