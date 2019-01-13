dc3449602a1eb339228669cf455136dfb8fd2bb7
Tony Jin <jsveron23@gmail.com>
2019-01-13 15:01:21 +0800

    Apply Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from '~/store'
 import App from '~/components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import {
+  createStore,
+  applyMiddleware
+} from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const composeMiddleware = (...middleware) => {
+  middleware = applyMiddleware(...middleware)
+
+  if (process.env.NODE_ENV !== 'production') {
+    return composeWithDevTools(middleware)
+  }
+
+  return middleware
+}
+
+const configureStore = (initialState = {}) => {
+  return createStore(
+    {},
+    initialState,
+    composeMiddleware(thunk)
+  )
+}
+
+const store = configureStore()
+store.withInitialState = configureStore
+
+export default store
