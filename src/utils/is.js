88ee2e0a0a6f0cd541c57f58947aaad3bc393020
Tony Jin <jsveron23@gmail.com>
2019-01-18 22:58:45 +0800

    Simplify isEmpty, isExit functions

--- src/utils/is.js
+++ src/utils/is.js
@@ -1,32 +1,31 @@
-function _isEmpty (isSome = false) {
-  function _is (v) {
-    if (typeof v === 'function' || typeof v === 'symbol') {
-      return false
-    }
-
-    if (typeof v === 'number' && Number.isNaN(v)) {
-      return true
-    }
-
-    return (
-      v === null ||
-      v === undefined ||
-      (v.hasOwnProperty('length') && v.length === 0) ||
-      (v.constructor === Object && Object.keys(v).length === 0)
-    )
+function emptyFn (v) {
+  if (typeof v === 'function' || typeof v === 'symbol') {
+    return false
   }
 
-  return (...x) => isSome
-    ? x.some(_is)
-    : x.every(_is)
+  if (typeof v === 'number' && Number.isNaN(v)) {
+    return true
+  }
+
+  return (
+    v === null ||
+    v === undefined ||
+    (v.hasOwnProperty('length') && v.length === 0) ||
+    (v.constructor === Object && Object.keys(v).length === 0)
+  )
+}
+
+function _isEmpty (isSome = false) {
+  const fnName = isSome ? 'some' : 'every'
+
+  return (...x) => x[fnName](emptyFn)
 }
 
 function _isExist (isSome = false) {
-  const _is = _isEmpty(isSome)
+  const fnName = isSome ? 'some' : 'every'
+  const cb = (v) => !emptyFn(v)
 
-  return (...x) => isSome
-    ? x.some((v) => !_is(v))
-    : x.every((v) => !_is(v))
+  return (...x) => x[fnName](cb)
 }
 
 /**
@@ -40,7 +39,4 @@ isEmpty.or = _isEmpty(true)
 const isExist = _isExist()
 isExist.or = _isExist(true)
 
-export {
-  isEmpty,
-  isExist
-}
+export { isEmpty, isExist }
