5143c345c53ccdda2c7515a26d435e004673aae2
Tony Jin <jsveron23@gmail.com>
2019-02-03 23:23:19 +0800

    Create transformNotationsToTiles function test case

--- src/chess/helpers/transformNotationsToTiles.js
+++ src/chess/helpers/transformNotationsToTiles.js
@@ -1,3 +1,8 @@
+/**
+ * Transform notations to tiles
+ * @param  {Array} notations
+ * @return {Array}
+ */
 function transformNotationsToTiles (notations) {
   return notations.map((notation) => notation.substr(2, 2))
 }
