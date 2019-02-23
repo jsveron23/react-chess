60ad57a0300ebaed7e5747f9d01699a818724f9a
Tony Jin <jsveron23@gmail.com>
2019-02-23 10:22:35 +0800

    Enhance createRegExp test case

--- tests/utils/createRegExp.spec.js
+++ tests/utils/createRegExp.spec.js
@@ -4,6 +4,8 @@ describe('#createRegExp', () => {
   it('create regular expression from string', () => {
     expect(createRegExp('a')).toEqual(/a/)
     expect(createRegExp('a?asb')).toEqual(/a?asb/)
+    expect(createRegExp('[a-z]|[1-9]')).toEqual(/[a-z]|[1-9]/)
     expect(createRegExp.withOptions('g')('a')).toEqual(/a/g)
+    expect(createRegExp.withOptions('mg')('ab')).toEqual(/ab/gm)
   })
 })
