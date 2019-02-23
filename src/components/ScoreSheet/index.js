62edf78a95018eb5f40d2e66fa32bdf89a5e5e22
Tony Jin <jsveron23@gmail.com>
2019-02-23 14:16:09 +0800

    Optimize ScoreSheet component

--- src/components/ScoreSheet/index.js
+++ src/components/ScoreSheet/index.js
@@ -1,43 +1,61 @@
-import React, { useEffect, useRef } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
+import { compose, difference } from 'ramda'
+import { isExist } from '~/utils'
 import css from './ScoreSheet.css'
 
-const ScoreSheet = ({ isDoingMatch, sheet }) => {
-  const len = sheet.length
-  const sheetRef = useRef(null)
-  const cls = cx(css.sheet, {
-    'is-doing-match': isDoingMatch
-  })
+class ScoreSheet extends Component {
+  static propTypes = {
+    sheet: PropTypes.array
+  }
 
-  useEffect(() => {
-    sheetRef.current.scrollTop = 0
-  })
+  static defaultProps = {
+    sheet: []
+  }
 
-  return (
-    <ul ref={sheetRef} className={cls}>
-      {sheet.map((item, idx) => {
-        const { white, black } = item
-        const key = `${idx}-${white}-${black}`
+  shouldComponentUpdate (nextProps) {
+    const prevProps = this.props
+    const shouldUpdate = compose(
+      isExist,
+      difference(nextProps.sheet)
+    )(prevProps.sheet)
 
-        return (
-          <li key={key} className="l-flex-row">
-            <span className={css.sheetIndex}>{len - idx}</span>
-            <span className={css.sheetWhite}>{white}</span>
-            <span className={css.sheetBlack}>{black}</span>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+    if (shouldUpdate) {
+      return true
+    }
 
-ScoreSheet.propTypes = {
-  sheet: PropTypes.array
-}
+    return false
+  }
 
-ScoreSheet.defaultProps = {
-  sheet: []
+  componentDidUpdate () {
+    this.sheetRef.scrollTop = 0
+  }
+
+  render () {
+    const { isDoingMatch, sheet } = this.props
+    const len = sheet.length
+    const cls = cx(css.sheet, {
+      'is-doing-match': isDoingMatch
+    })
+
+    return (
+      <ul ref={(el) => (this.sheetRef = el)} className={cls}>
+        {sheet.map((item, idx) => {
+          const { white, black } = item
+          const key = `${idx}-${white}-${black}`
+
+          return (
+            <li key={key} className="l-flex-row">
+              <span className={css.sheetIndex}>{len - idx}</span>
+              <span className={css.sheetWhite}>{white}</span>
+              <span className={css.sheetBlack}>{black}</span>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
 }
 
 export default ScoreSheet
