b96ec5f61619f0b3b88a1dac205798c36768dbf7
Tony Jin <jsveron23@gmail.com>
2019-03-11 01:33:27 +0900

    Optimize Webpack script (#44)

--- scripts/webpack/lib/path.js
+++ scripts/webpack/lib/path.js
@@ -1,12 +1,16 @@
 /**
  * Get absolute path
- * @param  {boolean}  isWin
- * @param  {string}   cwd
+ * @param  {Boolean?}  isWin
+ * @param  {String?}   cwd
  * @return {Function}
  */
 function createResolve (isWin, cwd) {
   const separator = isWin ? '\\' : '/'
 
+  /**
+   * @param  {...String} [...args]
+   * @return {String}
+   */
   return (...args) => {
     return args.reduce((acc, arg) => `${acc}${separator}${arg}`, cwd)
   }
@@ -16,5 +20,6 @@ const resolve = createResolve(process.platform === 'win32', process.cwd())
 
 module.exports = {
   resolve,
-  root: resolve()
+  root: resolve(),
+  webpack: resolve('scripts', 'webpack')
 }
