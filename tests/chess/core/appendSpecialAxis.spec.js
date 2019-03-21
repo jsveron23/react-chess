eb4960d54f2014a4fe6b9a359190d2ac2fd17583
Tony Jin <jsveron23@gmail.com>
2019-03-21 20:44:52 +0800

    Add no castling test case

--- tests/chess/core/appendSpecialAxis.spec.js
+++ tests/chess/core/appendSpecialAxis.spec.js
@@ -220,5 +220,34 @@ describe('#appendSpecialAxis', () => {
         appendSpecialAxis('w', specialK, 'e1', '', timeline, movableK)
       ).toEqual(expect.arrayContaining([[7, 1]]))
     })
+
+    it('castling: no castling', () => {
+      // prettier-ignore
+      const timeline = [
+        [
+          'bRa8', 'bNb8', 'bBc8', 'bQd8', 'bKe8', 'bBf8', 'bNg8', 'bRh8',
+          'bPa7', 'bPb7', 'bPc7', 'bPd7', 'bPe3', 'bPf7', 'bPg7', 'bPh7',
+          'wPa2', 'wPb2', 'wPc4', 'wPd2', 'wPe2', 'wPf5', 'wPg2', 'wPh2',
+          'wRa1', 'wNb1', 'wBc1', 'wQd1', 'wKe1', 'wBf1', 'wNg1', 'wRh1'
+        ]
+      ]
+
+      const pieceK = 'K'
+      const specialK = SPECIALS[pieceK]
+      const movableK = [
+        [5, 0],
+        [5, 2],
+        [4, 1],
+        [6, 1],
+        [4, 0],
+        [4, 2],
+        [6, 0],
+        [6, 2]
+      ]
+
+      expect(
+        appendSpecialAxis('w', specialK, 'e1', '', timeline, movableK)
+      ).toEqual(expect.arrayContaining([]))
+    })
   })
 })
