edfb03e8c78b4aad30aa759675f7f0d1deb334d6
Tony Jin <jsveron23@gmail.com>
2019-03-21 20:52:04 +0800

    Fixes test case

--- tests/chess/core/appendSpecialAxis.spec.js
+++ tests/chess/core/appendSpecialAxis.spec.js
@@ -227,7 +227,7 @@ describe('#appendSpecialAxis', () => {
         [
           'bRa8', 'bNb8', 'bBc8', 'bQd8', 'bKe8', 'bBf8', 'bNg8', 'bRh8',
           'bPa7', 'bPb7', 'bPc7', 'bPd7', 'bPe3', 'bPf7', 'bPg7', 'bPh7',
-          'wPa2', 'wPb2', 'wPc4', 'wPd2', 'wPe2', 'wPf5', 'wPg2', 'wPh2',
+          'wPa2', 'wPb2', 'wPc4', 'wPd2', 'wPe2', 'wPf2', 'wPg2', 'wPh2',
           'wRa1', 'wNb1', 'wBc1', 'wQd1', 'wKe1', 'wBf1', 'wNg1', 'wRh1'
         ]
       ]
@@ -247,7 +247,7 @@ describe('#appendSpecialAxis', () => {
 
       expect(
         appendSpecialAxis('w', specialK, 'e1', '', timeline, movableK)
-      ).toEqual(expect.arrayContaining([]))
+      ).toEqual(movableK)
     })
   })
 })
