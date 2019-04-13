bd6aca3a0cdec03dc8dbda802ee06ca16a7a58e3
Tony Jin <jsveron23@gmail.com>
2019-04-13 17:08:19 +0800

    Refactor decrease function test

--- src/utils/tests/createRegExp.spec.js
+++ src/utils/tests/createRegExp.spec.js
@@ -1,10 +1,11 @@
 import { createRegExp } from '~/utils'
 
 describe('#createRegExp', () => {
-  it('Create regular expression', () => {
+  test('Create regular expression', () => {
     expect(createRegExp('a')).toEqual(/a/)
     expect(createRegExp('a?asb')).toEqual(/a?asb/)
     expect(createRegExp('[a-z]|[1-9]')).toEqual(/[a-z]|[1-9]/)
+    expect(createRegExp.withOptions(null)('a')).toEqual(/a/)
     expect(createRegExp.withOptions('g')('a')).toEqual(/a/g)
     expect(createRegExp.withOptions('mg')('ab')).toEqual(/ab/gm)
   })
--- src/utils/tests/decrease.spec.js
+++ src/utils/tests/decrease.spec.js
@@ -1,12 +1,13 @@
 import { decrease } from '~/utils'
 
 describe('#decrease', () => {
-  describe('simple decrease', () => {
-    it('it should be returned array', () => {
+  describe('Simple decrease', () => {
+    test('it should be returned array', () => {
       expect(decrease(8, 4)).toEqual([8, 7, 6, 5])
+      expect(decrease(5, -2)).toEqual([5, 4, 3, 2, 1, 0, -1])
     })
 
-    it('it will not decrease', () => {
+    test('it will not decrease', () => {
       expect(decrease(4, 8)).toEqual([])
     })
   })
