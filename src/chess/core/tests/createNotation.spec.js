d5ca6f5f2607b6c85a7f8b7440bc9d05cfee33f0
Tony Jin <jsveron23@gmail.com>
2019-08-09 19:29:45 +0800

    Add throw test

--- src/chess/core/tests/createNotation.spec.js
+++ src/chess/core/tests/createNotation.spec.js
@@ -123,5 +123,25 @@ describe('#createNotation', () => {
 
       expect(createNotation([snapshotList[0]])).toEqual([])
     })
+
+    it('throw', () => {
+      // prettier-ignore
+      const snapshotList = [
+        [
+          'bRa8', 'bNb8', 'bBc8', 'bQd8', 'bKe8', 'bBf8', 'bRh8',
+          'bPa7', 'bPb7', 'bPc7', 'bPd7', 'bPe7', 'bPf7', 'bPg7', 'bPh7',
+          'wPa2', 'wPb2', 'wPc2', 'wPd2', 'wPe2', 'wPf2', 'wPg2', 'wPh2',
+          'wRa1', 'wBc1', 'wQd1', 'wKe1', 'wBf1', 'wNg1', 'wRh1'
+        ],
+        [
+          'bRa8', 'bNb8', 'bBc8', 'bQd8', 'bKe8', 'bBf8', 'bNg8', 'bRh8',
+          'bPa7', 'bPb7', 'bPc7', 'bPd7', 'bPe7', 'bPf7', 'bPg7', 'bPh7',
+          'wPa2', 'wPb2', 'wPc2', 'wPd2', 'wPe2', 'wPf2', 'wPg2', 'wPh2',
+          'wRa1', 'wNb1', 'wBc1', 'wQd1', 'wKe1', 'wBf1', 'wNg1', 'wRh1'
+        ]
+      ]
+
+      expect(() => createNotation(snapshotList)).toThrow()
+    })
   })
 })
