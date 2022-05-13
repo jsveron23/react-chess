5a16b45e03c3b32f3ffa6bbaa015fdb365cf299f
Tony Jin <jsveron23@gmail.com>
2022-05-13 12:02:54 +0900

    Clean packages bundling code for speed up

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import reactSvg from 'rollup-plugin-react-svg';
+import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 /** @see {@link https://github.com/reduxjs/redux/blob/master/rollup.config.js} */
@@ -14,47 +15,33 @@ function makeExternalPredicate(externalArr) {
   return (id) => pattern.test(id);
 }
 
-export default [
-  {
-    input: 'src/index.js',
-    output: {
+export default {
+  input: 'src/index.js',
+  output: [
+    {
       file: 'lib/index.js',
       format: 'cjs',
       sourcemap: true,
     },
-    external: makeExternalPredicate([
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ]),
-    plugins: [
-      nodeResolve(),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
-      }),
-      reactSvg(),
-    ],
-  },
-  {
-    input: 'src/index.js',
-    output: {
+    {
       dir: 'es',
       format: 'es',
       sourcemap: true,
       preserveModules: true,
       preserveModulesRoot: 'src',
     },
-    external: makeExternalPredicate([
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ]),
-    plugins: [
-      nodeResolve(),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
-      }),
-      reactSvg(),
-    ],
-  },
-];
+  ],
+  external: makeExternalPredicate([
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ]),
+  plugins: [
+    nodeResolve(),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+    }),
+    reactSvg(),
+    process.env.NODE_ENV === 'production' && uglify(),
+  ],
+};
